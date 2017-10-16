578eb5285c544189afd9f5f433aa13882f2b8e36
jason <jasonbolbar@hotmail.com>
2017-10-16 10:44:09 -0600

    set and get cookies

--- login.pl
+++ login.pl
@@ -1,13 +1,12 @@
 #!/usr/bin/perl -w
 
 use utils;
-
 main();
 
 sub main
 {
 	utils::addHeader();
 	$content = utils::getTemplate('templates/login.html');
-	print $content;
+	print $cookies{'jason'};
 }
 
--- utils.pm
+++ utils.pm
@@ -21,4 +21,28 @@ sub getTemplate {
 sub addHeader {
 	print "Content-type: text/html\n\n";
 }
+
+sub setCookie {
+	my ($cookieName, $cookieValue, $maxAge) = @_;
+	$maxAge = $maxAge ? $maxAge : 3600;
+	print "Set-Cookie: $cookieName=$cookieValue; Max-Age=$maxAge\n";
+}
+
+sub getCookies
+{
+  my %decode = ('\+'=>' ','\%3A\%3A'=>'::','\%26'=>'&','\%3D'=>'=',
+             '\%2C'=>',','\%3B'=>';','\%2B'=>'+','\%25'=>'%');
+
+  my %Cookies = ();
+  foreach (split(/; /,$ENV{'HTTP_COOKIE'})) {
+    my ($cookie,$value) = split(/=/);
+    foreach my $ch ('\+','\%3A\%3A','\%26','\%3D','\%2C','\%3B','\%2B','\
++%25') {
+      $cookie =~ s/$ch/$decode{$ch}/g;
+      $value =~ s/$ch/$decode{$ch}/g;
+    }
+    $Cookies{$cookie} = $value;
+  }
+  return %Cookies;
+}
 1;
