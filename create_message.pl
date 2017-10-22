a794e392f8c2f05ef098c0d59f65c96bfaf4b279
carolinalopez <carolinalopez2000@gmail.com>
2017-10-21 23:01:07 -0600

    Add message form

--- create_message.pl
+++ create_message.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/perl -w
 use Modules::Http::Request;
 use Modules::Database::Querier;
 use Modules::Authentication;
@@ -6,7 +7,17 @@ main();
 
 sub main
 {
-	my %message = Modules::Http::Request::getRequestData('POST');
+	my %params = Modules::Http::Request::getRequestData('POST');
 	my $user_id = Modules::Authentication::getSessionUserId();
-	my %row = Modules::Database::Querier::execute('CALL send_message(user_id, message, name, email, phone);');
+	print $user_id;
+	my %row = Modules::Database::Querier::execute(
+		'CALL send_message(?, ?, ?, ?, ?);', 
+		$user_id, $params{'message'}, $params{'name'}, $params{'email'}, $params{'phone'}
+	);
+
+	if ($row{'status'} == 0) {
+	   Modules::Http::Request::redirectTo('/support?alert=error');
+	} else {
+	   Modules::Http::Request::redirectTo('/?alert=success');
+	}
 }
