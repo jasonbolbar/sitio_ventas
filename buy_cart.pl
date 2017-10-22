c2dded39891edd9999c02e091cf96bb0989aefb8
carolinalopez <carolinalopez2000@gmail.com>
2017-10-22 13:27:02 -0600

    Add product list in cart

--- buy_cart.pl
+++ buy_cart.pl
@@ -5,28 +5,30 @@ main();
 
 sub main
 {
-	my $cart_id = getCartID();
-	
-	my $products_in_cart = Modules::Database::Querier::::execute('SELECT * FROM shopping_cart_products WHERE cart_id = ?;', $cart_id);
-	
-	if ($products{'status'} == 1 and @{$products{'rows'}} != 0) {
-		foreach $product in (@{$products_in_cart{'rows'}}) {
-			if ($product->{'quantity'} > $product->{'in_stock'}) {
-				Modules::Database::Querier::do('DELETE FROM items where cart_id = ? AND product_id = ?;', $cart_id, $product->{'id'});
-			} else {
-				$leftover = $product->{'in_stock'} - $product->{'quantity'};
-				Modules::Database::Querier::do('UPDATE products SET quantity = ? WHERE id = ?;', $leftover, $product->{'id'});
+	my $user_id = Modules::Authentication::getSessionUserId();
+	if ($user_id) {
+
+		my %cart = Modules::Database::Querier::::execute('SELECT id FROM active_shopping_carts WHERE user_id = ?', $user_id);
+		
+		if ($cart{'status'} = 1 and @{$cart{'rows'}} != 0) {
+			my $cart_id = $cart['id'];
+			
+			my %products_in_cart = Modules::Database::Querier::::execute('SELECT * FROM shopping_cart_products WHERE cart_id = ?;', $cart_id);
+			
+			if ($products{'status'} == 1 and @{$products{'rows'}} != 0) {
+				foreach $product in (@{$products_in_cart{'rows'}}) {
+					if ($product->{'quantity'} > $product->{'in_stock'}) {
+						Modules::Database::Querier::do('DELETE FROM items where cart_id = ? AND product_id = ?;', $cart_id, $product->{'id'});
+					} else {
+						my $leftover = $product->{'in_stock'} - $product->{'quantity'};
+						Modules::Database::Querier::do('UPDATE products SET quantity = ? WHERE id = ?;', $leftover, $product->{'id'});
+					}
+				}
+				Modules::Database::Querier::do('UPDATE shopping_carts SET finished = 0 WHERE id = ?;', $cart_id);
+				Modules::Http::Cookie::setCookie('Success', 'Productos comprados correctamente', 5);	
+				Modules::Http::Request::redirectTo('/');
 			}
 		}
-		Modules::Database::Querier::do('UPDATE shopping_carts SET finished = 0 WHERE id = ?;', $cart_id);
-		Modules::Http::Cookie::setCookie('Success', 'Productos comprados correctamente', 5);	
-		Modules::Http::Request::redirectTo('/');
 	}
 
-}
-
-sub getCartID 
-{
-	%parameters = Modules::Http::Request::getRequestData('POST');
-	return $parameters{'id'};
 }
--- shoppings.pl
+++ shoppings.pl
@@ -18,6 +18,40 @@ sub main
 		$content = Modules::Util::replace("<page-content>", Modules::Util::getFile('templates/shopping_cart.html') , $content);
 		$content = Modules::Util::replace("--title--", "Carrito de Compras", $content);
 		$content = Modules::Util::replace("--subtitle--", "Mis compras", $content);
+
+		$content = Modules::Util::replace("<cart-rows>", Modules::Util::getFile('templates/cart-row.html') , $content);
+
+		my $user_id = Modules::Authentication::getSessionUserId();
+		my %cart = Modules::Database::Querier::::execute('SELECT id FROM active_shopping_carts WHERE user_id = ?', $user_id);
+		
+		if ($cart{'status'} = 1 and @{$cart{'rows'}} != 0) {
+			my $rows;
+			my $cart_id = $cart['id'];
+			my %products = Modules::Database::Querier::::execute('SELECT * FROM shopping_cart_products WHERE cart_id = ?;', $cart_id);
+
+			if ($products{'status'} == 1 and @{$products{'rows'}} != 0) {
+				my $row;
+				my $no_product = '<span class="w3-tag w3-red w3-round">Agotado!</span>';
+				foreach  $result (@{$products{'rows'}}){
+					$row = Modules::Util::getFile('templates/cart-row.html');
+					$row = Modules::Util::replace('--name--', $result->{'name'}, $row);
+					$row = Modules::Util::replace('--quantity--', $result->{'quantity'}, $row);
+					if ($product->{'in_stock'} == 0) {
+						$row = Modules::Util::replace('--price--', $no_product, $row);
+					} else {
+						$row = Modules::Util::replace('--price--', $result->{'price'}, $row);
+					}
+					$row = Modules::Util::replace('--product-id--', $result->{'id'}, $row);
+					$rows .= $row;
+				}
+				
+			} else {	
+				$rows = Modules::Util::getFile('templates/search-no-results.html');
+			}
+
+			$content = Modules::Util::replace("<rows>", $rows, $content);
+		}
+
 		print $content;
 	} else 
 	{
